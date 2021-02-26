9e816cd858c8b09c607bd258663506dbd0dffb0d
ophir <pere.jobs@gmail.com>
2021-02-26 20:57:54 +0100

    v0

---
+++ src/lib.rs
@@ -0,0 +1,182 @@
+use std::convert::{TryFrom, TryInto};
+use std::ffi::c_void;
+use std::ops::{Bound, RangeBounds};
+use std::os::raw::c_int;
+
+use highs_sys::*;
+
+pub use status::{HighsModelStatus, HighsStatus};
+
+mod status;
+
+pub struct Row(c_int);
+
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct Problem {
+    // columns
+    colcost: Vec<f64>,
+    collower: Vec<f64>,
+    colupper: Vec<f64>,
+    // rows
+    rowlower: Vec<f64>,
+    rowupper: Vec<f64>,
+    // column-wise sparse constraints  matrix
+    astart: Vec<c_int>,
+    aindex: Vec<c_int>,
+    avalue: Vec<f64>,
+}
+
+impl Problem {
+    fn num_cols(&self) -> usize {
+        self.colcost.len()
+    }
+    fn num_rows(&self) -> usize {
+        self.rowlower.len()
+    }
+    fn num_non_zero(&self) -> usize {
+        self.avalue.len()
+    }
+    pub fn add_row<B: RangeBounds<f64>>(&mut self, bounds: B) -> Row {
+        let r = Row(self.num_rows().try_into().unwrap());
+        let low = bound_value(bounds.start_bound()).unwrap_or(f64::NEG_INFINITY);
+        let high = bound_value(bounds.start_bound()).unwrap_or(f64::INFINITY);
+        self.rowlower.push(low);
+        self.rowupper.push(high);
+        r
+    }
+}
+
+fn bound_value(b: Bound<&f64>) -> Option<f64> {
+    match b {
+        Bound::Included(v) => Some(*v),
+        Bound::Excluded(v) => Some(*v),
+        Bound::Unbounded => None
+    }
+}
+
+fn c(n: usize) -> c_int {
+    n.try_into().unwrap()
+}
+
+/*impl Default for Problem {
+    fn default() -> Self {
+        unimplemented!()
+    }
+}*/
+
+#[derive(Debug)]
+pub struct Model {
+    highs: *mut c_void,
+}
+
+#[derive(Debug)]
+pub struct SolvedModel {
+    highs: *mut c_void,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+pub enum Sense {
+    Maximise = -1,
+    Minimise = 1,
+}
+
+impl Model {
+    /// Create a Highs model to be optimized (but don't solve it yet).
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn set_sense(&mut self, sense: Sense) {
+        unsafe {
+            Highs_changeObjectiveSense(self.highs, sense as c_int);
+        }
+    }
+
+    pub fn set_problem(&mut self, problem: Problem) {
+        unsafe {
+            log::debug!("Adding a problem with {} variables and {} constraints to HiGHS",
+                        problem.num_cols(),
+                        problem.num_rows()
+            );
+            handle_status(Highs_passLp(
+                self.highs,
+                c(problem.num_cols()),
+                c(problem.num_rows()),
+                c(problem.num_non_zero()),
+                problem.colcost.as_ptr(),
+                problem.collower.as_ptr(),
+                problem.colupper.as_ptr(),
+                problem.rowlower.as_ptr(),
+                problem.rowupper.as_ptr(),
+                problem.astart.as_ptr(),
+                problem.aindex.as_ptr(),
+                problem.avalue.as_ptr(),
+            ));
+        }
+    }
+
+    /// Prevents writing anything to the standard output when solving the model
+    pub fn make_quiet(&mut self) {
+        handle_status(unsafe { Highs_runQuiet(self.highs) })
+    }
+
+    /// Find the optimal value for the problem
+    pub fn solve(self) -> SolvedModel {
+        unsafe {
+            handle_status(Highs_run(self.highs));
+        }
+        SolvedModel { highs: self.highs }
+    }
+}
+
+impl From<SolvedModel> for Model {
+    fn from(solved: SolvedModel) -> Self {
+        Self { highs: solved.highs }
+    }
+}
+
+impl Default for Model {
+    fn default() -> Self {
+        unsafe {
+            let highs = Highs_create();
+            Self { highs }
+        }
+    }
+}
+
+impl Drop for Model {
+    fn drop(&mut self) {
+        unsafe { Highs_destroy(self.highs) }
+    }
+}
+
+
+impl SolvedModel {
+    pub fn status(&self) -> HighsModelStatus {
+        use std::convert::TryFrom;
+        let model_status = unsafe { Highs_getModelStatus(self.highs, 0) };
+        HighsModelStatus::try_from(model_status).unwrap()
+    }
+}
+
+fn handle_status(status: c_int) {
+    match HighsStatus::try_from(status).unwrap() {
+        HighsStatus::OK => {}
+        HighsStatus::Warning => { log::warn!("Warning from HiGHS !"); }
+        HighsStatus::Error => { panic!("An error was encountered in HiGHS. This is probably a memory allocation error."); }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn solve_problem_empty() {
+        let mut model = Model::default();
+        model.set_problem(Problem::default());
+        let solved = model.solve();
+        assert_eq!(solved.status(), HighsModelStatus::ModelEmpty);
+    }
+}
---
+++ src/status.rs
@@ -0,0 +1,81 @@
+use std::convert::TryFrom;
+use std::fmt::{Debug, Formatter};
+use std::os::raw::c_int;
+
+#[derive(Clone, Copy, Debug, PartialOrd, PartialEq, Ord, Eq)]
+pub enum HighsModelStatus {
+    NotSet = 0,
+    LoadError = 1,
+    ModelError = 2,
+    PresolveError = 3,
+    SolveError = 4,
+    PostsolveError = 5,
+    ModelEmpty = 6,
+    PrimalInfeasible = 7,
+    PrimalUnbounded = 8,
+    Optimal = 9,
+    ReachedDualObjectiveValueUpperBound = 10,
+    ReachedTimeLimit = 11,
+    ReachedIterationLimit = 12,
+    PrimalDualInfeasible = 13,
+    DualInfeasible = 14,
+}
+
+#[derive(PartialEq, Clone, Copy)]
+pub struct InvalidStatus(pub c_int);
+
+impl Debug for InvalidStatus {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} is not a valid HiGHS model status. \
+        This error comes from a bug in highs rust bindings. \
+        Please report it.", self.0)
+    }
+}
+
+impl TryFrom<c_int> for HighsModelStatus {
+    type Error = InvalidStatus;
+
+    fn try_from(value: c_int) -> Result<Self, Self::Error> {
+        use highs_sys::*;
+        match value {
+            MODEL_STATUS_NOTSET => Ok(Self::NotSet),
+            MODEL_STATUS_LOAD_ERROR => Ok(Self::LoadError),
+            MODEL_STATUS_MODEL_ERROR => Ok(Self::ModelError),
+            MODEL_STATUS_PRESOLVE_ERROR => Ok(Self::PresolveError),
+            MODEL_STATUS_SOLVE_ERROR => Ok(Self::SolveError),
+            MODEL_STATUS_POSTSOLVE_ERROR => Ok(Self::PostsolveError),
+            MODEL_STATUS_MODEL_EMPTY => Ok(Self::ModelEmpty),
+            MODEL_STATUS_PRIMAL_INFEASIBLE => Ok(Self::PrimalInfeasible),
+            MODEL_STATUS_PRIMAL_UNBOUNDED => Ok(Self::PrimalUnbounded),
+            MODEL_STATUS_OPTIMAL => Ok(Self::Optimal),
+            MODEL_STATUS_REACHED_DUAL_OBJECTIVE_VALUE_UPPER_BOUND => Ok(Self::ReachedDualObjectiveValueUpperBound),
+            MODEL_STATUS_REACHED_TIME_LIMIT => Ok(Self::ReachedTimeLimit),
+            MODEL_STATUS_REACHED_ITERATION_LIMIT => Ok(Self::ReachedIterationLimit),
+            MODEL_STATUS_PRIMAL_DUAL_INFEASIBLE => Ok(Self::PrimalDualInfeasible),
+            MODEL_STATUS_DUAL_INFEASIBLE => Ok(Self::DualInfeasible),
+            n => Err(InvalidStatus(n))
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialOrd, PartialEq, Ord, Eq)]
+pub enum HighsStatus {
+    OK = 0,
+    Warning = 1,
+    Error = 2,
+}
+
+
+impl TryFrom<c_int> for HighsStatus {
+    type Error = InvalidStatus;
+
+    fn try_from(value: c_int) -> Result<Self, InvalidStatus> {
+        use highs_sys::*;
+        match value {
+            STATUS_OK => Ok(Self::OK),
+            STATUS_WARNING => Ok(Self::Warning),
+            STATUS_ERROR => Ok(Self::Error),
+            n => Err(InvalidStatus(n))
+        }
+    }
+}
