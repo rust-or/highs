0f4c9b6991b78893edbe81bd3babdc4d612ceb44
ophir <pere.jobs@gmail.com>
2021-02-28 10:57:11 +0100

    Fix bugs and add tests

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub use status::{HighsModelStatus, HighsStatus};
 
 mod status;
 
+#[derive(Debug, Clone, Copy)]
 pub struct Row(c_int);
 
 #[derive(Debug, Clone, PartialEq, Default)]
@@ -43,7 +44,7 @@ impl Problem {
     pub fn add_row<B: RangeBounds<f64>>(&mut self, bounds: B) -> Row {
         let r = Row(self.num_rows().try_into().unwrap());
         let low = bound_value(bounds.start_bound()).unwrap_or(f64::NEG_INFINITY);
-        let high = bound_value(bounds.start_bound()).unwrap_or(f64::INFINITY);
+        let high = bound_value(bounds.end_bound()).unwrap_or(f64::INFINITY);
         self.rowlower.push(low);
         self.rowupper.push(high);
         r
@@ -52,7 +53,7 @@ impl Problem {
     /// Add a column (a variable) to the problem.
     /// col_factor represents the factor in front of the variable in the objective function.
     /// The row_factors argument defines how much this variable weights in each constraint.
-    pub fn add_column<B: RangeBounds<f64>, I: IntoIterator<Item = (Row, f64)>>(
+    pub fn add_column<N: Into<f64> + Copy, B: RangeBounds<N>, I: IntoIterator<Item=(Row, f64)>>(
         &mut self,
         col_factor: f64,
         bounds: B,
@@ -60,7 +61,7 @@ impl Problem {
     ) {
         self.colcost.push(col_factor);
         let low = bound_value(bounds.start_bound()).unwrap_or(f64::NEG_INFINITY);
-        let high = bound_value(bounds.start_bound()).unwrap_or(f64::INFINITY);
+        let high = bound_value(bounds.end_bound()).unwrap_or(f64::INFINITY);
         self.collower.push(low);
         self.colupper.push(high);
         self.astart.push(self.aindex.len().try_into().unwrap());
@@ -71,10 +72,10 @@ impl Problem {
     }
 }
 
-fn bound_value(b: Bound<&f64>) -> Option<f64> {
+fn bound_value<N: Into<f64> + Copy>(b: Bound<&N>) -> Option<f64> {
     match b {
-        Bound::Included(v) => Some(*v),
-        Bound::Excluded(v) => Some(*v),
+        Bound::Included(v) => Some((*v).into()),
+        Bound::Excluded(v) => Some((*v).into()),
         Bound::Unbounded => None,
     }
 }
@@ -83,20 +84,15 @@ fn c(n: usize) -> c_int {
     n.try_into().unwrap()
 }
 
-/*impl Default for Problem {
-    fn default() -> Self {
-        unimplemented!()
-    }
-}*/
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Model {
-    highs: *mut c_void,
+    highs: HighsPtr,
 }
 
 #[derive(Debug)]
 pub struct SolvedModel {
-    highs: *mut c_void,
+    highs: HighsPtr,
 }
 
 #[repr(C)]
@@ -114,7 +110,7 @@ impl Model {
 
     pub fn set_sense(&mut self, sense: Sense) {
         unsafe {
-            Highs_changeObjectiveSense(self.highs, sense as c_int);
+            Highs_changeObjectiveSense(self.highs.mut_ptr(), sense as c_int);
         }
     }
 
@@ -126,7 +122,7 @@ impl Model {
                 problem.num_rows()
             );
             handle_status(Highs_passLp(
-                self.highs,
+                self.highs.mut_ptr(),
                 c(problem.num_cols()),
                 c(problem.num_rows()),
                 c(problem.num_non_zero()),
@@ -144,13 +140,13 @@ impl Model {
 
     /// Prevents writing anything to the standard output when solving the model
     pub fn make_quiet(&mut self) {
-        handle_status(unsafe { Highs_runQuiet(self.highs) })
+        handle_status(unsafe { Highs_runQuiet(self.highs.mut_ptr()) })
     }
 
     /// Find the optimal value for the problem
-    pub fn solve(self) -> SolvedModel {
+    pub fn solve(mut self) -> SolvedModel {
         unsafe {
-            handle_status(Highs_run(self.highs));
+            handle_status(Highs_run(self.highs.mut_ptr()));
         }
         SolvedModel { highs: self.highs }
     }
@@ -164,36 +160,55 @@ impl From<SolvedModel> for Model {
     }
 }
 
-impl Default for Model {
-    fn default() -> Self {
-        unsafe {
-            let highs = Highs_create();
-            Self { highs }
-        }
+#[derive(Debug)]
+struct HighsPtr(*mut c_void);
+
+impl Drop for HighsPtr {
+    fn drop(&mut self) {
+        unsafe { Highs_destroy(self.0) }
     }
 }
 
-impl Drop for Model {
-    fn drop(&mut self) {
-        unsafe { Highs_destroy(self.highs) }
+impl Default for HighsPtr {
+    fn default() -> Self {
+        HighsPtr(unsafe { Highs_create() })
+    }
+}
+
+impl HighsPtr {
+    // To be used instead of unsafe_mut_ptr wherever possible
+    #[allow(dead_code)]
+    fn ptr(&self) -> *const c_void {
+        self.0
+    }
+
+    // Needed until https://github.com/ERGO-Code/HiGHS/issues/479 is fixed
+    unsafe fn unsafe_mut_ptr(&self) -> *mut c_void {
+        self.0
+    }
+
+    fn mut_ptr(&mut self) -> *mut c_void {
+        self.0
     }
 }
 
 impl SolvedModel {
     pub fn status(&self) -> HighsModelStatus {
-        let model_status = unsafe { Highs_getModelStatus(self.highs, 0) };
+        let model_status = unsafe { Highs_getModelStatus(self.highs.unsafe_mut_ptr(), 0) };
         HighsModelStatus::try_from(model_status).unwrap()
     }
     pub fn get_solution(&self) -> Solution {
-        let mut colvalue: Vec<f64> = vec![0.; self.num_cols()];
-        let mut coldual: Vec<f64> = vec![0.; self.num_cols()];
-        let mut rowvalue: Vec<f64> = vec![0.; self.num_rows()];
-        let mut rowdual: Vec<f64> = vec![0.; self.num_rows()];
+        let cols = self.num_cols();
+        let rows = self.num_rows();
+        let mut colvalue: Vec<f64> = vec![0.; cols];
+        let mut coldual: Vec<f64> = vec![0.; cols];
+        let mut rowvalue: Vec<f64> = vec![0.; rows];
+        let mut rowdual: Vec<f64> = vec![0.; rows];
 
         // Get the primal and dual solution
         unsafe {
             Highs_getSolution(
-                self.highs,
+                self.highs.unsafe_mut_ptr(),
                 colvalue.as_mut_ptr(),
                 coldual.as_mut_ptr(),
                 rowvalue.as_mut_ptr(),
@@ -210,16 +225,17 @@ impl SolvedModel {
     }
 
     fn num_cols(&self) -> usize {
-        let n = unsafe { Highs_getNumCols(self.highs) };
+        let n = unsafe { Highs_getNumCols(self.highs.unsafe_mut_ptr()) };
         n.try_into().unwrap()
     }
 
     fn num_rows(&self) -> usize {
-        let n = unsafe { Highs_getNumRows(self.highs) };
+        let n = unsafe { Highs_getNumRows(self.highs.unsafe_mut_ptr()) };
         n.try_into().unwrap()
     }
 }
 
+#[derive(Clone, Debug)]
 pub struct Solution {
     colvalue: Vec<f64>,
     coldual: Vec<f64>,
@@ -266,5 +282,35 @@ mod tests {
         model.set_problem(Problem::default());
         let solved = model.solve();
         assert_eq!(solved.status(), HighsModelStatus::ModelEmpty);
+    }
+
+    #[test]
+    fn solve_sum() {
+        // max: x + 2y + z
+        // under constraints:
+        // c1: 3x +  y      <= 6
+        // c2:       y + 2z <= 7
+        let mut pb = Problem::default();
+        let c1 = pb.add_row(..6.);
+        let c2 = pb.add_row(..7.);
+        // x
+        pb.add_column(1., 0.., vec![(c1, 3.)]);
+        // y
+        pb.add_column(2., 0.., vec![(c1, 1.), (c2, 1.)]);
+        // z
+        pb.add_column(1., 0.., vec![(c2, 2.)]);
+        let mut model = Model::default();
+        model.set_problem(pb);
+        model.set_sense(Sense::Maximise);
+
+        let solved = model.solve();
+
+        assert_eq!(solved.status(), HighsModelStatus::Optimal);
+
+        let solution = solved.get_solution();
+        // The expected solution is x=0  y=6  z=0.5
+        assert_eq!(solution.columns(), vec![0., 6., 0.5]);
+        // All the constraints are at their maximum
+        assert_eq!(solution.rows(), vec![6., 7.]);
     }
 }
